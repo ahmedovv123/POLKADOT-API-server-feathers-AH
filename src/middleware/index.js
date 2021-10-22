4fcfe287cb8fdd6e104d11d29f8c12089df167cd
Ahmet <ahmedovv123@gmail.com>
2021-10-22 14:43:53 +0300

    feat: accounts api

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,13 @@
-// eslint-disable-next-line no-unused-vars
+const apiConnection = require('../../nodeConnection');
+
+const connectApi = apiConnection.getNodeConnection().then((api) => {
+    return api;
+});
+
 module.exports =  function (app) {
   // Add your custom middleware here. Remember that
   // in Express, the order matters.
-
+  
   ///////////////// TRANSACTIONS //////////////////////
 
   app.get('/api/transactions/count', async function (req, res) {
@@ -34,4 +39,42 @@ module.exports =  function (app) {
     } });
     res.send(result);
   })
+
+  ///////////////// ACCOUNTS //////////////////////
+
+  app.get('/api/accounts/transactions/count/:accountId', async function (req, res) {
+    const accountId = req.params.accountId
+    const result  = await app.service('transactions').find({
+      query: { 
+        $or: [
+          {sender: accountId},
+          {recipient: accountId}
+        ],
+        $limit: 0
+      }
+    })
+    res.send(result);
+  })
+
+  app.get('/api/accounts/transactions/:accountId', async function (req, res) {
+    const accountId = req.params.accountId
+    const result  = await app.service('transactions').find({
+      query: { 
+        $or: [
+          {sender: accountId},
+          {recipient: accountId}
+        ],
+        $limit: 50
+      }
+    })
+    res.send(result);
+  })
+
+  app.get('/api/accounts/balance/:accountId', async function (req, res) {
+    const accountId = req.params.accountId
+    const result = await connectApi.then(api => api.query.system.account(accountId));
+    res.send(result);
+  })
+
+  
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,6 @@
 const transactions = require('./transactions/transactions.service.js');
 const polkadotExplorer = require('./polkadot_explorer/polkadot_explorer.service.js');
+
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(transactions);
