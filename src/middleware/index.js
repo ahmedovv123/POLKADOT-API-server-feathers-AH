93fe43d3a24bb59f85a6e76e81cccf070212ec9e
Ahmet <ahmedovv123@gmail.com>
2021-10-22 15:54:05 +0300

    fix tx api

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
   /// ////////////// TRANSACTIONS //////////////////////
 
   app.get('/api/transactions/count', async function (req, res) {
-    const result = await app.service('transactions').find({ query: { $limit: 1 } })
+    const result = await app.service('transactions').find({ query: { $limit: 0 } })
     res.send(result)
   })
 
